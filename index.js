9bb8625f30b72db64ee60e22256c996cc27afb8d
Vortex <gj.tewatia24@gmail.com>
2023-02-20 13:41:09 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const http = require("http");                                // requiring in-built HTTP module
+
+const server = http.createServer((req, res) => {             // creating HTTP server
+    res.write("Hello World!!");
+    res.end();
+});
+
+server.listen(3000, () => {                                  // Setting up server at port 3000
+    console.log("Server is up and running on port 3000");
+});
